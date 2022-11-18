79c0df1f74708fa82172d9b334ef6c7996add21e
giovani <giovanimoresco@gmail.com>
2022-11-18 11:19:59 -0300

    UsuarioController.cs DONE

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using ECommerceWebService.Data;
+using ECommerceWebService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWebService.Controllers
+{
+	public class UsuarioController : Controller
+	{
+		private DataContext _ctx;
+		public UsuarioController(DataContext ctx)
+		{
+			_ctx = ctx;
+		}
+
+		public IActionResult CadastrarUsuario([FromBody]Usuario usuario)
+		{
+			_ctx.Usuarios.Add(usuario);
+			_ctx.SaveChanges();
+			return Created("OK", usuario);
+		}
+
+		public IActionResult AlterarUsuario([FromBody]Usuario usuario)
+		{
+			_ctx.Usuarios.Update(usuario);
+			_ctx.SaveChanges();
+			return Ok(usuario);
+		}
+
+		public IActionResult DeletarUsuario([FromBody] Usuario usuario)
+		{
+			_ctx.Usuarios.Remove(usuario);
+			_ctx.SaveChanges();
+			return Ok();
+		}
+
+		public IActionResult ListarUsuarios() => Ok(_ctx.Usuarios.ToList());
+		public IActionResult BuscarUsuarioPorId([FromRoute]int id) => Ok(_ctx.Usuarios.Find(id));
+	}
+}
