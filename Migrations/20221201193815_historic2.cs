419ad67386dfee55662d0e8dc72294b8b62e5eb7
giovanimds <giovanimoresco@gmail.com>
2022-12-01 19:12:08 -0300

    c

---
+++ Migrations/20221201193815_historic2.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerceWebService.Migrations
+{
+    public partial class historic2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Historico_ProdutoId",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProdutoId",
+                table: "Produtos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.CreateTable(
+                name: "HistoricoProduto",
+                columns: table => new
+                {
+                    HistoricosHistoricoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProdutosProdutoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoricoProduto", x => new { x.HistoricosHistoricoId, x.ProdutosProdutoId });
+                    table.ForeignKey(
+                        name: "FK_HistoricoProduto_Historico_HistoricosHistoricoId",
+                        column: x => x.HistoricosHistoricoId,
+                        principalTable: "Historico",
+                        principalColumn: "HistoricoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HistoricoProduto_Produtos_ProdutosProdutoId",
+                        column: x => x.ProdutosProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "ProdutoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoricoProduto_ProdutosProdutoId",
+                table: "HistoricoProduto",
+                column: "ProdutosProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoricoProduto");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProdutoId",
+                table: "Produtos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Historico_ProdutoId",
+                table: "Produtos",
+                column: "ProdutoId",
+                principalTable: "Historico",
+                principalColumn: "HistoricoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
